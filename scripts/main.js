2f47a83ffcb2d357bb9520074994230b4d69a85c
DESKTOP-RVTL3QT\GilS <games.gil@gmail.com>
2018-07-21 23:06:52 +0300

    fixed redraw bug

--- scripts/main.js
+++ scripts/main.js
@@ -2,25 +2,28 @@ const move = _ => {
     View.clear()
     Game.move()
 
-
-    if (Game.isEating()) {
+    if (Game.isHit()) {
+        stop()
+    } else if (Game.isEating()) {
         Game.removeFood()
         Game.addOne()
         Game.addFood()
-    } else if (Game.isHit()) {
-        stop()
+
+        View.draw(Game.getFoodIdx(), "food")
+        Game.getSnake().map(pos =>View.draw(pos, "snake"))
+
     } else {
         if (!Game.getSnake().filter(pos => 
             pos.x < 0 || pos.x >= Game.getGrid().length ||
             pos.y < 0 || pos.y >= Game.getGrid().length).length) {
                 Game.getSnake().map(pos => View.draw(pos, "snake"))
     
-                if (Game.getFoodIdx() >= 0) {
-                    View.draw(Game.getFoodIdx(), "food")
-                }
-            } else {
-                stop()
+            if (Game.getFoodIdx() >= 0) {
+                View.draw(Game.getFoodIdx(), "food")
             }
+        } else {
+            stop()
+        }
     }
 
     
